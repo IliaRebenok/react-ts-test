7c53f56974368b2aa29ec4c0ec0eda975cf036ef
Ilia Rebenok <rebenok.ilya13@gmail.com>
2023-02-16 13:32:19 +0700

    starting the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [number, setNumber] = useState <number> (5);
+
+  const changeNumber = () => {
+    setNumber('10')
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1
     </div>
   );
 }
